ebd82089688b5b4a40f6883d1af94a441eaa05d3
Eray AYDIN <eray@erayaydin.me>
2014-11-24 16:33:57 +0200

    UsersTableSeeder reformat

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -9,21 +9,39 @@ class UsersTableSeeder extends Seeder {
         \Eloquent::unguard();
 		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
-		{
-            $username = $faker->userName;
-            $email    = $faker->email;
-            $password = $faker->password;
-			User::create([
-                "username" => $username,
-                "email"    => $email,
-                "password" => $password
-			]);
-            $this->command->info("# New registered user #");
-            $this->command->info("Username : ".$username);
-            $this->command->info("Email    : ".$email);
-            $this->command->info("Password : ".$password);
-		}
+        $num = $this->command->ask("How many you want add user? ");
+
+        if(!is_numeric($num)){
+            $this->command->error($num." is not numeric!");
+            return false;
+        } else {
+            if($num > 500){
+                if(!$this->command->confirm("You want add ".$num." user to table. Do you wish to continue? [yes|no]")){
+                    $this->command->error("Canceled");
+                    return false;
+                }
+                $confirm = true;
+            } else {
+                $confirm = true;
+            }
+        }
+
+        if($confirm){
+            foreach (range(1, $num) as $index) {
+                $username = $faker->userName;
+                $email = $faker->email;
+                $password = $faker->password;
+                User::create([
+                    "username" => $username,
+                    "email" => $email,
+                    "password" => $password
+                ]);
+                $this->command->info("# New registered user #");
+                $this->command->info("Username : " . $username);
+                $this->command->info("Email    : " . $email);
+                $this->command->info("Password : " . $password);
+            }
+        }
 	}
 
 }
